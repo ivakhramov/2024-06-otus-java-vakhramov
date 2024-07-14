d680034e98bc4d90a74931d6a89213a15650e3c9
ivakhramov <ivan.vakhramov@outlook.com>
2024-07-14 15:14:37 +0300

    init

---
+++ src/main/java/ru/ivakhramov/java/basic/homeworks/GreetingsApplication.java
@@ -0,0 +1,7 @@
+package ru.ivakhramov.java.basic.homeworks;
+
+public class GreetingsApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
