0e058cc0cfd8ee9459286a1fbe0130c9e4e6fa22
ivakhramov <ivan.vakhramov@outlook.com>
2024-07-15 18:47:55 +0300

    Фикс - правка условия в методе selectColor() (замечание преподавателя)

--- src/main/java/ru/ivakhramov/java/basic/homeworks/lesson3/HomeworkLesson3.java
+++ src/main/java/ru/ivakhramov/java/basic/homeworks/lesson3/HomeworkLesson3.java
@@ -75,7 +75,7 @@ public class HomeworkLesson3 {
         // System.out.println("data = " + data); // Вывод в консоль для проверки
         if (data <= 10) {
             System.out.println("Красный");
-        } else if (data > 10 && data <= 20) {
+        } else if (data <= 20) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
