cd0392773beacc0879e869d3cab6238f59b7c631
ivakhramov <ivan.vakhramov@outlook.com>
2024-09-11 23:53:06 +0300

    Домашняя работа к уроку 16 готова

---
+++ src/main/java/ru/ivakhramov/java/basic/homeworks/lesson16/HomeworkLesson16Main.java
@@ -0,0 +1,25 @@
+package ru.ivakhramov.java.basic.homeworks.lesson16;
+
+public class HomeworkLesson16Main {
+
+    public static void main(String[] args) {
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add("Иванов Иван Иванович", "+7 911 111-11-11");
+        phoneBook.add("Иванов Петр Петрович", "+7 911 111-11-12");
+        phoneBook.add("Петров Петр Петрович", "+7 911 111-11-13");
+        phoneBook.add("Петров Петр Петрович", "+7 911 111-11-14");
+        System.out.println(phoneBook); // Иванов Петр Петрович=[+7 911 111-11-12],
+                                       // Петров Петр Петрович=[+7 911 111-11-13, +7 911 111-11-14],
+                                       // Иванов Иван Иванович=[+7 911 111-11-11]
+
+        System.out.println(phoneBook.find("Иванов Иван Иванович")); // [+7 911 111-11-11]
+        System.out.println(phoneBook.find("Петров Петр Петрович")); // [+7 911 111-11-13, +7 911 111-11-14]
+
+        System.out.println(phoneBook.containsPhoneNumber("+7 911 111-11-14")); // true
+        System.out.println(phoneBook.containsPhoneNumber("+7 911 111-11-15")); // false
+    }
+
+
+}
---
+++ src/main/java/ru/ivakhramov/java/basic/homeworks/lesson16/PhoneBook.java
@@ -0,0 +1,51 @@
+package ru.ivakhramov.java.basic.homeworks.lesson16;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// 1. Реализуйте класс PhoneBook, который хранит в себе список имен (фио) и телефонных номеров
+public class PhoneBook {
+
+    // 4. Под одним именем может быть несколько телефонов
+    // (в случае однофамильцев, или наличии у одного человека нескольких номеров),
+    // тогда при запросе такой фамилии должны выводиться все телефоны
+    private Map<String, List<String>> phoneBook;
+
+    PhoneBook() {
+        phoneBook = new HashMap<>();
+    }
+
+    @Override
+    public String toString() {
+        return phoneBook.toString();
+    }
+
+    // 2. Метод add() должен позволять добавлять запись имя-телефон
+    public void add(String name, String phoneNumber) {
+
+        phoneBook.putIfAbsent(name, new ArrayList<>());
+        phoneBook.get(name).add(phoneNumber);
+    }
+
+    // 3. Метод find() выполнять поиск номер(-а, -ов) телефона по имени
+    public List<String> find(String name) {
+
+        return phoneBook.get(name);
+    }
+
+    // 5. Метод containsPhoneNumber должен проверять наличие телефона в справочнике
+    public boolean containsPhoneNumber(String phoneNumber) {
+
+        boolean result = false;
+        for (List<String> phoneNumbers : phoneBook.values()) {
+            if (phoneNumbers.contains(phoneNumber)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+}
