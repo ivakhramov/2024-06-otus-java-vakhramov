4ba882c4401ebbe668fe10d193f576f864a17dc3
ivakhramov <ivan.vakhramov@outlook.com>
2024-11-06 21:07:44 +0300

    Фикс - исправление форматирования

--- src/main/java/ru/ivakhramov/java/basic/homeworks/lesson24/DDL.sql
+++ src/main/java/ru/ivakhramov/java/basic/homeworks/lesson24/DDL.sql
@@ -4,27 +4,27 @@ CREATE DATABASE otus_java_basic_homework_lesson24;
 
 CREATE TABLE public.tests
 (
-    id serial,
-    name varchar(100) NOT NULL,
-    description text NOT NULL,
+    id          serial,
+    name        varchar(100) NOT NULL,
+    description text         NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE public.questions
 (
-    id serial,
-    question text NOT NULL,
-    test_id integer NOT NULL,
+    id       serial,
+    question text    NOT NULL,
+    test_id  integer NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (test_id) REFERENCES public.tests(id) ON DELETE CASCADE
+    FOREIGN KEY (test_id) REFERENCES public.tests (id) ON DELETE CASCADE
 );
 
 CREATE TABLE public.answers
 (
-    id serial,
-    answer text NOT NULL,
-    is_correct boolean NOT NULL,
+    id          serial,
+    answer      text    NOT NULL,
+    is_correct  boolean NOT NULL,
     question_id integer NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (question_id) REFERENCES public.questions(id) ON DELETE CASCADE
+    FOREIGN KEY (question_id) REFERENCES public.questions (id) ON DELETE CASCADE
 );
