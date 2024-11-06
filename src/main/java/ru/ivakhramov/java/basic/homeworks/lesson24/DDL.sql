7bc05b93e6fa35ce4fdb06c4a6a0bf7bf15f315a
ivakhramov <ivan.vakhramov@outlook.com>
2024-11-06 21:00:14 +0300

    Фикс - исправление замечаний в PR к ДЗ к уроку 24 готова

--- src/main/java/ru/ivakhramov/java/basic/homeworks/lesson24/DDL.sql
+++ src/main/java/ru/ivakhramov/java/basic/homeworks/lesson24/DDL.sql
@@ -4,27 +4,27 @@ CREATE DATABASE otus_java_basic_homework_lesson24;
 
 CREATE TABLE public.tests
 (
-    id serial NOT NULL,
-    test_name varchar(100) NOT NULL,
+    id serial,
+    name varchar(100) NOT NULL,
     description text NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE public.questions
 (
-    id serial NOT NULL,
+    id serial,
     question text NOT NULL,
-    id_test integer NOT NULL,
+    test_id integer NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_test) REFERENCES public.tests(id) ON DELETE CASCADE
+    FOREIGN KEY (test_id) REFERENCES public.tests(id) ON DELETE CASCADE
 );
 
 CREATE TABLE public.answers
 (
-    id serial NOT NULL,
+    id serial,
     answer text NOT NULL,
     is_correct boolean NOT NULL,
-    id_question integer NOT NULL,
+    question_id integer NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_question) REFERENCES public.questions(id) ON DELETE CASCADE
+    FOREIGN KEY (question_id) REFERENCES public.questions(id) ON DELETE CASCADE
 );
